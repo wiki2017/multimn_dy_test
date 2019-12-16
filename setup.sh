2e13c8fc563da364ed60dc92f064f3489d39d1b1
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2019-12-16 12:34:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -223,7 +223,7 @@ ExecStart=/usr/local/bin/iond -daemon -conf=$CONF_DIR/ioncoin.conf -datadir=$CON
 ExecStop=/usr/local/bin/ion-cli -conf=$CONF_DIR/ioncoin.conf -datadir=$CONF_DIR stop
 Restart=always
 PrivateTmp=true
-RestartSec=1
+TimeoutStartSec=10m
 StartLimitInterval=0
 [Install]
 WantedBy=multi-user.target
