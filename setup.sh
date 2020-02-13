155a0c401e8f08e4ebdef25906b812c8c48e88a7
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2020-02-14 00:23:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,7 +44,7 @@ echo "/swapfile none swap sw 0 0" >> /etc/fstab
 
 fi
   #wget https://github.com/wagerr/wagerr/releases/download/v3.0.1/wagerr-3.0.1-x86_64-linux-gnu.tar.gz
-  wget https://github.com/Streamies/Streamies/releases/download/v2.1/Streamies-2.1.0-x86_64-pc-linux-gnu.zip
+  wget https://github.com/Streamies/Streamies/releases/download/v2.2/Streamies-2.2.0-x86_64-pc-linux-gnu.zip
   #wget https://github.com/wagerr/Wagerr-Blockchain-Snapshots/releases/download/Block-826819/826819.zip -O bootstrap.zip
   #export fileid=1cCOIb-Xqs_MxsIPiwEAtvQps_6R1Ww13
   #export filename=Streamies-2.0.0-x86_64-pc-linux-gnu.zip
@@ -61,7 +61,7 @@ fi
 
   #wget --load-cookies cookies.txt -O $filename \
   #   'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-  unzip Streamies-2.1.0-x86_64-pc-linux-gnu.zip
+  unzip Streamies-2.2.0-x86_64-pc-linux-gnu.zip
   sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   sudo apt -y update
   sudo apt -y upgrade
@@ -77,7 +77,7 @@ fi
   chmod +x streamies-cli
   sudo cp  streamiesd /usr/local/bin
   sudo cp  streamies-cli /usr/local/bin
-  rm -rf Streamies-2.1.0-x86_64-pc-linux-gnu.zip
+  rm -rf Streamies-2.2.0-x86_64-pc-linux-gnu.zip
   
   sudo apt install -y ufw
   sudo ufw allow ssh/tcp
