9c2a3569b8d60b9192ee74970b267f8f01ad2c9d
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2020-06-17 23:29:21 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ echo "/swapfile none swap sw 0 0" >> /etc/fstab
 
 fi
  
-  wget https://github.com/Streamies/Streamies/releases/download/v2.3/Streamies-2.3.0.0-x86_64-pc-linux-gnu.zip
+  wget https://github.com/Streamies/Streamies/releases/download/v2.4.1/Streamies-v2.4.1-x86_64-pc-linux-gnu.zip
   
   export fileid=1gGiqVkJRDvPmhY_5v3_mlcIq617T1euB
   export filename=bootstrap.zip
@@ -59,13 +59,13 @@ fi
 
   wget --load-cookies cookies.txt -O $filename \
      'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-  unzip Streamies-2.3.0.0-x86_64-pc-linux-gnu.zip
+  unzip Streamies-v2.4.1-x86_64-pc-linux-gnu.zip
   
   chmod +x streamiesd
   chmod +x streamies-cli
   sudo cp  streamiesd /usr/local/bin
   sudo cp  streamies-cli /usr/local/bin
-  rm -rf Streamies-2.3.0.0-x86_64-pc-linux-gnu.zip
+  rm -rf Streamies-v2.4.1-x86_64-pc-linux-gnu.zip
   rm -rf streamies-cli
   rm -rf streamiesd
   rm -rf streamies-tx
